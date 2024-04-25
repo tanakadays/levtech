55d8b2b2ad5e7577187fb229683600529ae73a6a
tanakadays <12brillo.239yuyatnk.a.kappa9ri@gmail.com>
2024-04-25 20:11:18 +0900

    8-6投稿削除機能の追加

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,17 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     
     protected $fillable = [
         'title',
         'body'
         ];
     
-    public function getPaginateByLimit(int $limit_count = 10)
+    public function getPaginateByLimit(int $limit_count = 5)
     {
     // updated_atで降順に並べたあと、limitで件数制限をかける
     return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
