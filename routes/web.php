e815164a0865cf7ab456a7d9cd0adab0cb0c31b4
tanakadays <12brillo.239yuyatnk.a.kappa9ri@gmail.com>
2024-04-25 10:24:21 +0900

    8-3投稿記事画面作成

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,9 @@ use App\Http\Controllers\PostController;
 #Route::get('/posts', [PostController::class, 'index']);
 
 Route::get('/', [PostController::class, 'index']);
-
+Route::get('/posts/create', [PostController::class,'create']);
 Route::get('/posts/{post}', [PostController::class, 'show']);
+
 
 
 /*
