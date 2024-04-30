50e109d6060b90cb5d0bdfc38cb5ee1c3702fc63
tanakadays <12brillo.239yuyatnk.a.kappa9ri@gmail.com>
2024-04-30 15:01:48 +0900

    9-4完了

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,7 +11,7 @@
         <x-slot name="header">
             index
         </x-slot>
-        <body>
+    <body>
         <h1>Blog Name</h1>
         <a href="/posts/create">create</a>
         <div class='posts'>
@@ -44,7 +44,17 @@
                 }
             }
         </script>
-    </body>
+        
+        <div>
+        @foreach($questions as $question)
+            <div>
+                <a href="https://teratail.com/questions/{{ $question['id'] }}">
+                    {{ $question['title'] }}
+                </a>
+            </div>
+        @endforeach
+    </div>
+     </body>
     </x-app-layout>
     
 </html>
